5ff2d4ee9a1f3cbef3a0ad661df23e9caf85dec3
arunkumarrl <51299769+arunkumarrl@users.noreply.github.com>
2020-02-24 19:03:37 +0100

    Axios reinstalled

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,11 +5,11 @@ const axios = require('axios');
 module.exports.getAccessToken = async (event) => {
 
   const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
-    + '?client_id=AKIAJOPHWK4KMYYXWZ3Q#'
-    + '&client_secret=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'
+    + '?client_id=vogprrbq5kb6f74tbtsjntihku'
+    + '&client_secret=g0369ccq44km3nbt13epf2l0er'
     + '&grant_type=authorization_code'
     + '&redirect_uri=https://arunkumarrl.github.io/meetup/'
-    + '&code=https://secure.meetup.com/oauth2/authorize?client_id=vogprrbq5kb6f74tbtsjntihku&response_type=code&redirect_uri=https://arunkumarrl.github.io/;'
+    + '&code=' + event.pathParameters.code;
 
   const info = await axios.post(MEETUP_OAUTH_URL);
 
